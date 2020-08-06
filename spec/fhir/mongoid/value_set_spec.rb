86a24df1a9f6cc811d60c6ba238ab6e11fd7704f
Serhii Ilin <serhii.ilin@semanticbits.com>
2020-08-06 14:35:52 -0400

    MAT-1496 Codacy build failure fix

--- spec/fhir/mongoid/value_set_spec.rb
+++ spec/fhir/mongoid/value_set_spec.rb
@@ -16,13 +16,17 @@ RSpec.describe CQM::ValueSet do
     value_set = FHIR::ValueSet.transform_json value_set_hash
 
     updated_value_set_json = value_set.to_json
-    puts updated_value_set_json
     updated_value_set_hash =  JSON.load updated_value_set_json
+
+    expect(DateTime.parse(value_set_hash['meta']['lastUpdated'])).to eq  DateTime.parse(updated_value_set_hash['meta']['lastUpdated'])
+    value_set_hash['meta'].delete('lastUpdated')
+    updated_value_set_hash['meta'].delete('lastUpdated')
 
     expect(DateTime.parse(value_set_hash['date'])).to eq  DateTime.parse(updated_value_set_hash['date'])
     # Date formats are different
     value_set_hash.delete('date')
     updated_value_set_hash.delete('date')
+
     # Assert can be parsed
     updated_value_set = FHIR::ValueSet.transform_json updated_value_set_hash
     expect(updated_value_set).to be_present
