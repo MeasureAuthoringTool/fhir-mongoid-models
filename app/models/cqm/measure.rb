b81a253df33859f94706f08818b15e65b91c8c6e
Joe Kotanchik <joseph.kotanchik@semanticbits.com>
2020-08-10 17:40:41 -0400

    Add attributes for displaying cms id, title, and description to cqm measure model.

--- app/models/cqm/measure.rb
+++ app/models/cqm/measure.rb
@@ -20,6 +20,11 @@ module CQM
 
     CQL_SKIP_STATEMENTS = ['SDE Ethnicity', 'SDE Payer', 'SDE Race', 'SDE Sex'].freeze
 
+    # A CMS-style string (e.g. "CMS2v4") for the measure
+    field :cms_id, type: String
+    field :title, type: String, default: ''
+    field :description, type: String, default: ''
+
     # A version-specific UUID for the measure
     field :set_id, type: String
     field :calculation_method, type: String, default: 'PATIENT'
@@ -37,5 +42,7 @@ module CQM
     field :main_cql_library, type: String
 
     embeds_many :source_data_criteria, class_name: 'CQM::DataElement'
+
+    embeds_many :population_sets
   end
 end
