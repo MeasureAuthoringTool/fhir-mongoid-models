76b1928f346b89c95bde6f51d807603d4f785b63
danmee10 <danmee10@gmail.com>
2020-08-12 09:24:43 -0600

    reset auto generated file to previous state

--- app/models/fhir/library.rb
+++ app/models/fhir/library.rb
@@ -2,177 +2,177 @@ module FHIR
   # fhir/library.rb
   class Library < DomainResource
     include Mongoid::Document
-    embeds_one :url, class_name: 'FHIR::PrimitiveUri'
-    embeds_many :identifier, class_name: 'FHIR::Identifier'
-    embeds_one :version, class_name: 'FHIR::PrimitiveString'
-    embeds_one :name, class_name: 'FHIR::PrimitiveString'
-    embeds_one :title, class_name: 'FHIR::PrimitiveString'
-    embeds_one :subtitle, class_name: 'FHIR::PrimitiveString'
-    embeds_one :status, class_name: 'FHIR::PublicationStatus'
-    embeds_one :experimental, class_name: 'FHIR::PrimitiveBoolean'
-    embeds_one :type, class_name: 'FHIR::CodeableConcept'
-    embeds_one :subjectCodeableConcept, class_name: 'FHIR::CodeableConcept'
-    embeds_one :subjectReference, class_name: 'FHIR::Reference'
-    embeds_one :date, class_name: 'FHIR::PrimitiveDateTime'
-    embeds_one :publisher, class_name: 'FHIR::PrimitiveString'
-    embeds_many :contact, class_name: 'FHIR::ContactDetail'
-    embeds_one :description, class_name: 'FHIR::PrimitiveMarkdown'
-    embeds_many :useContext, class_name: 'FHIR::UsageContext'
-    embeds_many :jurisdiction, class_name: 'FHIR::CodeableConcept'
-    embeds_one :purpose, class_name: 'FHIR::PrimitiveMarkdown'
-    embeds_one :usage, class_name: 'FHIR::PrimitiveString'
-    embeds_one :copyright, class_name: 'FHIR::PrimitiveMarkdown'
-    embeds_one :approvalDate, class_name: 'FHIR::PrimitiveDate'
-    embeds_one :lastReviewDate, class_name: 'FHIR::PrimitiveDate'
-    embeds_one :effectivePeriod, class_name: 'FHIR::Period'
-    embeds_many :topic, class_name: 'FHIR::CodeableConcept'
-    embeds_many :author, class_name: 'FHIR::ContactDetail'
-    embeds_many :editor, class_name: 'FHIR::ContactDetail'
-    embeds_many :reviewer, class_name: 'FHIR::ContactDetail'
-    embeds_many :endorser, class_name: 'FHIR::ContactDetail'
-    embeds_many :relatedArtifact, class_name: 'FHIR::RelatedArtifact'
-    embeds_many :parameter, class_name: 'FHIR::ParameterDefinition'
-    embeds_many :dataRequirement, class_name: 'FHIR::DataRequirement'
-    embeds_many :content, class_name: 'FHIR::Attachment'
-
+    embeds_one :url, class_name: 'FHIR::PrimitiveUri'    
+    embeds_many :identifier, class_name: 'FHIR::Identifier'    
+    embeds_one :version, class_name: 'FHIR::PrimitiveString'    
+    embeds_one :name, class_name: 'FHIR::PrimitiveString'    
+    embeds_one :title, class_name: 'FHIR::PrimitiveString'    
+    embeds_one :subtitle, class_name: 'FHIR::PrimitiveString'    
+    embeds_one :status, class_name: 'FHIR::PublicationStatus'    
+    embeds_one :experimental, class_name: 'FHIR::PrimitiveBoolean'    
+    embeds_one :type, class_name: 'FHIR::CodeableConcept'    
+    embeds_one :subjectCodeableConcept, class_name: 'FHIR::CodeableConcept'    
+    embeds_one :subjectReference, class_name: 'FHIR::Reference'    
+    embeds_one :date, class_name: 'FHIR::PrimitiveDateTime'    
+    embeds_one :publisher, class_name: 'FHIR::PrimitiveString'    
+    embeds_many :contact, class_name: 'FHIR::ContactDetail'    
+    embeds_one :description, class_name: 'FHIR::PrimitiveMarkdown'    
+    embeds_many :useContext, class_name: 'FHIR::UsageContext'    
+    embeds_many :jurisdiction, class_name: 'FHIR::CodeableConcept'    
+    embeds_one :purpose, class_name: 'FHIR::PrimitiveMarkdown'    
+    embeds_one :usage, class_name: 'FHIR::PrimitiveString'    
+    embeds_one :copyright, class_name: 'FHIR::PrimitiveMarkdown'    
+    embeds_one :approvalDate, class_name: 'FHIR::PrimitiveDate'    
+    embeds_one :lastReviewDate, class_name: 'FHIR::PrimitiveDate'    
+    embeds_one :effectivePeriod, class_name: 'FHIR::Period'    
+    embeds_many :topic, class_name: 'FHIR::CodeableConcept'    
+    embeds_many :author, class_name: 'FHIR::ContactDetail'    
+    embeds_many :editor, class_name: 'FHIR::ContactDetail'    
+    embeds_many :reviewer, class_name: 'FHIR::ContactDetail'    
+    embeds_many :endorser, class_name: 'FHIR::ContactDetail'    
+    embeds_many :relatedArtifact, class_name: 'FHIR::RelatedArtifact'    
+    embeds_many :parameter, class_name: 'FHIR::ParameterDefinition'    
+    embeds_many :dataRequirement, class_name: 'FHIR::DataRequirement'    
+    embeds_many :content, class_name: 'FHIR::Attachment'    
+    
     def as_json(*args)
-      result = super
-      unless self.url.nil?
+      result = super      
+      unless self.url.nil? 
         result['url'] = self.url.value
-        serialized = Extension.serializePrimitiveExtension(self.url)
+        serialized = Extension.serializePrimitiveExtension(self.url)            
         result['_url'] = serialized unless serialized.nil?
       end
-      unless self.identifier.nil?  || !self.identifier.any?
+      unless self.identifier.nil?  || !self.identifier.any? 
         result['identifier'] = self.identifier.map{ |x| x.as_json(*args) }
       end
-      unless self.version.nil?
+      unless self.version.nil? 
         result['version'] = self.version.value
-        serialized = Extension.serializePrimitiveExtension(self.version)
+        serialized = Extension.serializePrimitiveExtension(self.version)            
         result['_version'] = serialized unless serialized.nil?
       end
-      unless self.name.nil?
+      unless self.name.nil? 
         result['name'] = self.name.value
-        serialized = Extension.serializePrimitiveExtension(self.name)
+        serialized = Extension.serializePrimitiveExtension(self.name)            
         result['_name'] = serialized unless serialized.nil?
       end
-      unless self.title.nil?
+      unless self.title.nil? 
         result['title'] = self.title.value
-        serialized = Extension.serializePrimitiveExtension(self.title)
+        serialized = Extension.serializePrimitiveExtension(self.title)            
         result['_title'] = serialized unless serialized.nil?
       end
-      unless self.subtitle.nil?
+      unless self.subtitle.nil? 
         result['subtitle'] = self.subtitle.value
-        serialized = Extension.serializePrimitiveExtension(self.subtitle)
+        serialized = Extension.serializePrimitiveExtension(self.subtitle)            
         result['_subtitle'] = serialized unless serialized.nil?
       end
-      unless self.status.nil?
+      unless self.status.nil? 
         result['status'] = self.status.value
-        serialized = Extension.serializePrimitiveExtension(self.status)
+        serialized = Extension.serializePrimitiveExtension(self.status)            
         result['_status'] = serialized unless serialized.nil?
       end
-      unless self.experimental.nil?
+      unless self.experimental.nil? 
         result['experimental'] = self.experimental.value
-        serialized = Extension.serializePrimitiveExtension(self.experimental)
+        serialized = Extension.serializePrimitiveExtension(self.experimental)            
         result['_experimental'] = serialized unless serialized.nil?
       end
-      unless self.type.nil?
+      unless self.type.nil? 
         result['type'] = self.type.as_json(*args)
       end
       unless self.subjectCodeableConcept.nil?
-        result['subjectCodeableConcept'] = self.subjectCodeableConcept.as_json(*args)
-      end
+        result['subjectCodeableConcept'] = self.subjectCodeableConcept.as_json(*args)                        
+      end          
       unless self.subjectReference.nil?
-        result['subjectReference'] = self.subjectReference.as_json(*args)
-      end
-      unless self.date.nil?
+        result['subjectReference'] = self.subjectReference.as_json(*args)                        
+      end          
+      unless self.date.nil? 
         result['date'] = self.date.value
-        serialized = Extension.serializePrimitiveExtension(self.date)
+        serialized = Extension.serializePrimitiveExtension(self.date)            
         result['_date'] = serialized unless serialized.nil?
       end
-      unless self.publisher.nil?
+      unless self.publisher.nil? 
         result['publisher'] = self.publisher.value
-        serialized = Extension.serializePrimitiveExtension(self.publisher)
+        serialized = Extension.serializePrimitiveExtension(self.publisher)            
         result['_publisher'] = serialized unless serialized.nil?
       end
-      unless self.contact.nil?  || !self.contact.any?
+      unless self.contact.nil?  || !self.contact.any? 
         result['contact'] = self.contact.map{ |x| x.as_json(*args) }
       end
-      unless self.description.nil?
+      unless self.description.nil? 
         result['description'] = self.description.value
-        serialized = Extension.serializePrimitiveExtension(self.description)
+        serialized = Extension.serializePrimitiveExtension(self.description)            
         result['_description'] = serialized unless serialized.nil?
       end
-      unless self.useContext.nil?  || !self.useContext.any?
+      unless self.useContext.nil?  || !self.useContext.any? 
         result['useContext'] = self.useContext.map{ |x| x.as_json(*args) }
       end
-      unless self.jurisdiction.nil?  || !self.jurisdiction.any?
+      unless self.jurisdiction.nil?  || !self.jurisdiction.any? 
         result['jurisdiction'] = self.jurisdiction.map{ |x| x.as_json(*args) }
       end
-      unless self.purpose.nil?
+      unless self.purpose.nil? 
         result['purpose'] = self.purpose.value
-        serialized = Extension.serializePrimitiveExtension(self.purpose)
+        serialized = Extension.serializePrimitiveExtension(self.purpose)            
         result['_purpose'] = serialized unless serialized.nil?
       end
-      unless self.usage.nil?
+      unless self.usage.nil? 
         result['usage'] = self.usage.value
-        serialized = Extension.serializePrimitiveExtension(self.usage)
+        serialized = Extension.serializePrimitiveExtension(self.usage)            
         result['_usage'] = serialized unless serialized.nil?
       end
-      unless self.copyright.nil?
+      unless self.copyright.nil? 
         result['copyright'] = self.copyright.value
-        serialized = Extension.serializePrimitiveExtension(self.copyright)
+        serialized = Extension.serializePrimitiveExtension(self.copyright)            
         result['_copyright'] = serialized unless serialized.nil?
       end
-      unless self.approvalDate.nil?
+      unless self.approvalDate.nil? 
         result['approvalDate'] = self.approvalDate.value
-        serialized = Extension.serializePrimitiveExtension(self.approvalDate)
+        serialized = Extension.serializePrimitiveExtension(self.approvalDate)            
         result['_approvalDate'] = serialized unless serialized.nil?
       end
-      unless self.lastReviewDate.nil?
+      unless self.lastReviewDate.nil? 
         result['lastReviewDate'] = self.lastReviewDate.value
-        serialized = Extension.serializePrimitiveExtension(self.lastReviewDate)
+        serialized = Extension.serializePrimitiveExtension(self.lastReviewDate)            
         result['_lastReviewDate'] = serialized unless serialized.nil?
       end
-      unless self.effectivePeriod.nil?
+      unless self.effectivePeriod.nil? 
         result['effectivePeriod'] = self.effectivePeriod.as_json(*args)
       end
-      unless self.topic.nil?  || !self.topic.any?
+      unless self.topic.nil?  || !self.topic.any? 
         result['topic'] = self.topic.map{ |x| x.as_json(*args) }
       end
-      unless self.author.nil?  || !self.author.any?
+      unless self.author.nil?  || !self.author.any? 
         result['author'] = self.author.map{ |x| x.as_json(*args) }
       end
-      unless self.editor.nil?  || !self.editor.any?
+      unless self.editor.nil?  || !self.editor.any? 
         result['editor'] = self.editor.map{ |x| x.as_json(*args) }
       end
-      unless self.reviewer.nil?  || !self.reviewer.any?
+      unless self.reviewer.nil?  || !self.reviewer.any? 
         result['reviewer'] = self.reviewer.map{ |x| x.as_json(*args) }
       end
-      unless self.endorser.nil?  || !self.endorser.any?
+      unless self.endorser.nil?  || !self.endorser.any? 
         result['endorser'] = self.endorser.map{ |x| x.as_json(*args) }
       end
-      unless self.relatedArtifact.nil?  || !self.relatedArtifact.any?
+      unless self.relatedArtifact.nil?  || !self.relatedArtifact.any? 
         result['relatedArtifact'] = self.relatedArtifact.map{ |x| x.as_json(*args) }
       end
-      unless self.parameter.nil?  || !self.parameter.any?
+      unless self.parameter.nil?  || !self.parameter.any? 
         result['parameter'] = self.parameter.map{ |x| x.as_json(*args) }
       end
-      unless self.dataRequirement.nil?  || !self.dataRequirement.any?
+      unless self.dataRequirement.nil?  || !self.dataRequirement.any? 
         result['dataRequirement'] = self.dataRequirement.map{ |x| x.as_json(*args) }
       end
-      unless self.content.nil?  || !self.content.any?
+      unless self.content.nil?  || !self.content.any? 
         result['content'] = self.content.map{ |x| x.as_json(*args) }
       end
       result.delete('id')
       unless self.fhirId.nil?
         result['id'] = self.fhirId
         result.delete('fhirId')
-      end
+      end  
       result
     end
 
     def self.transform_json(json_hash, target = Library.new)
-
+    
       result = self.superclass.transform_json(json_hash, target)
       result['url'] = PrimitiveUri.transform_json(json_hash['url'], json_hash['_url']) unless json_hash['url'].nil?
       result['identifier'] = json_hash['identifier'].map { |var| Identifier.transform_json(var) } unless json_hash['identifier'].nil?
