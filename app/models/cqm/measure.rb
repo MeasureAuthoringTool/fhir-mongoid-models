fc014ad14e248a2b7df5f9e651db5d1d69120d0a
Serhii Ilin <serhii.ilin@semanticbits.com>
2020-08-12 14:07:51 -0400

    MAT-1566 1. mongoid wrapper classes JSON serialization / deserialization 2. Drop CQM::ValueSet

--- app/models/cqm/measure.rb
+++ app/models/cqm/measure.rb
@@ -32,7 +32,7 @@ module CQM
 
     embeds_one :fhir_measure, class_name: 'FHIR::Measure'
     embeds_many :libraries, class_name: 'FHIR::Library'
-    embeds_many :value_sets, class_name: 'CQM::ValueSet'
+    embeds_many :value_sets, class_name: 'FHIR::ValueSet'
     has_one :package, class_name: 'CQM::MeasurePackage', inverse_of: :measure, dependent: :destroy
 
     # ELM/CQL Measure-logic related data
@@ -42,5 +42,73 @@ module CQM
     field :main_cql_library, type: String
 
     embeds_many :source_data_criteria, class_name: 'CQM::DataElement'
+
+    def as_json(*args)
+      result = Hash.new
+      unless self.id.nil?
+        result['id'] = self.id.to_s
+      end
+      unless self.cms_id.nil?
+        result['cms_id'] = self.cms_id
+      end
+      unless self.title.nil?
+        result['title'] = self.title
+      end
+      unless self.description.nil?
+        result['description'] = self.description
+      end
+      unless self.set_id.nil?
+        result['set_id'] = self.set_id
+      end
+      unless self.calculation_method.nil?
+        result['calculation_method'] = self.calculation_method
+      end
+      unless self.calculate_sdes.nil?
+        result['calculate_sdes'] = self.calculate_sdes
+      end
+      unless self.fhir_measure.nil?
+        result['fhir_measure'] = self.fhir_measure.as_json(*args)
+      end
+      unless self.libraries.nil?  || !self.libraries.any?
+        result['libraries'] = self.libraries.map{ |x| x.as_json(*args) }
+      end
+      unless self.value_sets.nil?  || !self.value_sets.any?
+        result['value_sets'] = self.value_sets.map{ |x| x.as_json(*args) }
+      end
+      unless self.cql_libraries.nil?  || !self.cql_libraries.any?
+        result['cql_libraries'] = self.cql_libraries.map{ |x| x.as_json(*args) }
+      end
+      unless self.main_cql_library.nil?
+        result['main_cql_library'] = self.main_cql_library
+      end
+      unless self.source_data_criteria.nil?  || !self.source_data_criteria.any?
+        result['source_data_criteria'] = self.source_data_criteria.map{ |x| x.as_json(*args) }
+      end
+      result['created_at'] = self.created_at unless self.created_at.nil?
+      result['updated_at'] = self.updated_at unless self.updated_at.nil?
+
+      result
+    end
+
+    def self.transform_json(json_hash, target = Measure.new)
+      result = target
+      result['id'] = json_hash['id'] unless json_hash['id'].nil?
+      result['cms_id'] = json_hash['cms_id'] unless json_hash['cms_id'].nil?
+      result['title'] = json_hash['title'] unless json_hash['title'].nil?
+      result['description'] = json_hash['description'] unless json_hash['description'].nil?
+      result['set_id'] = json_hash['set_id'] unless json_hash['set_id'].nil?
+      result['calculation_method'] = json_hash['calculation_method'] unless json_hash['calculation_method'].nil?
+      result['calculate_sdes'] = json_hash['calculate_sdes'] unless json_hash['calculate_sdes'].nil?
+      result['fhir_measure'] = FHIR::Measure.transform_json(json_hash['fhir_measure']) unless json_hash['fhir_measure'].nil?
+      result['libraries'] = json_hash['libraries'].map { |var| FHIR::Library.transform_json(var) } unless json_hash['libraries'].nil?
+      result['value_sets'] = json_hash['value_sets'].map { |var| FHIR::ValueSet.transform_json(var) } unless json_hash['value_sets'].nil?
+      result['cql_libraries'] = json_hash['cql_libraries'].map { |var| CQM::LogicLibrary.transform_json(var) } unless json_hash['cql_libraries'].nil?
+      result['main_cql_library'] = json_hash['main_cql_library'] unless json_hash['main_cql_library'].nil?
+      result['source_data_criteria'] = json_hash['source_data_criteria'].map { |var| CQM::DataElement.transform_json(var) } unless json_hash['cql_libraries'].nil?
+      result['created_at'] = json_hash['created_at'] unless json_hash['created_at'].nil?
+      result['updated_at'] = json_hash['updated_at'] unless json_hash['updated_at'].nil?
+      result
+    end
   end
+
 end
--- app/models/cqm/patient.rb
+++ app/models/cqm/patient.rb
@@ -10,5 +10,45 @@ module CQM
 
     embeds_one :fhir_patient, class_name: 'FHIR::Patient', autobuild: true
     embeds_many :data_elements, class_name: 'CQM::DataElement'
+
+    def as_json(*args)
+      result = Hash.new
+      unless self.id.nil?
+        result['id'] = self.id.to_s
+      end
+      unless self.expected_values.nil?
+        result['expected_values'] = self.expected_values
+      end
+      unless self.notes.nil?
+        result['notes'] = self.notes
+      end
+      unless self.measure_ids.nil?
+        result['measure_ids'] = self.measure_ids
+      end
+      unless self.fhir_patient.nil?
+        result['fhir_patient'] = self.fhir_patient.as_json(*args)
+      end
+      unless self.data_elements.nil?  || !self.data_elements.any?
+        result['data_elements'] = self.data_elements.map{ |x| x.as_json(*args) }
+      end
+      result['created_at'] = self.created_at unless self.created_at.nil?
+      result['updated_at'] = self.updated_at unless self.updated_at.nil?
+
+      result
+    end
+
+    def self.transform_json(json_hash, target = Patient.new)
+      result = target
+      result['id'] = json_hash['id'] unless json_hash['id'].nil?
+      result['expected_values'] = json_hash['expected_values'] unless json_hash['expected_values'].nil?
+      result['notes'] = json_hash['notes'] unless json_hash['notes'].nil?
+      result['description'] = json_hash['description'] unless json_hash['description'].nil?
+      result['measure_ids'] = json_hash['measure_ids'] unless json_hash['measure_ids'].nil?
+      result['fhir_patient'] = FHIR::Patient.transform_json(json_hash['fhir_patient']) unless json_hash['fhir_patient'].nil?
+      result['data_elements'] = json_hash['data_elements'].map { |var| CQM::DataElement.transform_json(var) } unless json_hash['data_elements'].nil?
+      result['created_at'] = json_hash['created_at'] unless json_hash['created_at'].nil?
+      result['updated_at'] = json_hash['updated_at'] unless json_hash['updated_at'].nil?
+      result
+    end
   end
 end
